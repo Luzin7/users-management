600a8a4c15ddca9a8031e8c05034112151bc66a7
Luan Victor <lvictor.dutra@gmail.com>
2025-06-22 13:18:21 -0300

    fix: correct statusCode in UnauthorizedError to UNAUTHORIZED

--- src/shared/errors/UnauthorizedError.ts
+++ src/shared/errors/UnauthorizedError.ts
@@ -2,7 +2,7 @@ import { ServiceError } from '@shared/core/errors/ServiceError';
 import { statusCode } from '@shared/core/types/statusCode';
 
 export class UnauthorizedError extends Error implements ServiceError {
-  statusCode: number = statusCode.FORBIDDEN;
+  statusCode: number = statusCode.UNAUTHORIZED;
 
   constructor() {
     super('Você não possui permissão para executar essa ação');
